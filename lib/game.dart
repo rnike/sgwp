dd0222df61b000228df2d039d15753e467d87c44
m6503 <m6503@dongzhande-MacBook-Pro.local>
2019-05-09 00:16:37 +0800

    ok

--- lib/game.dart
+++ lib/game.dart
@@ -4,6 +4,7 @@ import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
+//
 class MyGame extends BaseGame {
   MyGame() {
     init();
@@ -15,14 +16,22 @@ class MyGame extends BaseGame {
   @override
   void resize(Size size) {
     GameValues.gameSize = size;
+    super.resize(size);
   }
 }
+
+
 
 class Background extends Component {
   @override
   void render(Canvas c) {
     if (GameValues.gameSize == null) return;
-    c.drawRect(Offset.zero & GameValues.gameSize, Paint()..color = Colors.blue);
+
+    //背景尺寸，使用 BaseGame resize 的值
+    final backgroundSize = Offset.zero & GameValues.gameSize;
+    
+    //畫出簡單的純藍色背景
+    c.drawRect(backgroundSize, Paint()..color = Colors.blue);
   }
   @override
   void update(double t) {
@@ -30,16 +39,19 @@ class Background extends Component {
   }
 }
 
+//通用變數放一起
 class GameValues {
   static Size gameSize;
   static int score;
   static int heightScore;
 }
 
-class GameObject extends Component {
-  static const double WIDTH = 50;
-  static const double HEIGHT = 50;
+enum ObjectType { people, goods }
 
+class GameObject extends Component {
+  static const Size OBJ_SIZE = Size(50,50);
+  final ObjectType type;
+  GameObject(this.type);
   @override
   void render(Canvas c) {
     // TODO: implement render
@@ -51,10 +63,13 @@ class GameObject extends Component {
   }
 }
 
-enum ObjectType { people, goods }
 
 //人出得去
-class People extends GameObject {}
+class People extends GameObject {
+  People({ObjectType type=ObjectType.people}) : super(type);
+}
 
 //貨進得來
-class Goods extends GameObject {}
+class Goods extends GameObject {
+  Goods({ObjectType type=ObjectType.goods}) : super(type);
+}
